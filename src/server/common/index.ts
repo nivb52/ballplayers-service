98173aafa3cdddd6bbb6de0125b7682fd69133a5
Niv <nivb52@gmail.com>
2021-11-17 08:57:56 +0200

    add default headers

--- src/server/common/index.ts
+++ src/server/common/index.ts
@@ -2,3 +2,11 @@ import type { Request, Response } from "express";
 
 export const defaultResponseError = (res: Response): Response =>
   res.status(500).send("something went wrong");
+
+
+  // export const headers = (lastUpdated) => {
+  //   headers: {
+  //         'Content-Type': 'application/json',
+  //         'Last-Modified': new Date(lastUpdated).toUTCString()
+  //       }
+  // }
